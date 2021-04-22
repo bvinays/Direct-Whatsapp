40fef5781ab7c530e6c085111ce6614522e3e278
vinay singh <53850778+bvinays@users.noreply.github.com>
2021-04-22 16:54:39 +0530

    chaanges

--- main.js
+++ main.js
@@ -4,7 +4,9 @@
 function sendMessage(){
    const ccode=$('#country_list').get(0).value
    const mobnumber=$('#inputMobNum').get(0).value
-    window.location.href="https://wa.me/"
+   const message=$('#inputMessage').get(0).value
+   const href="https://web.whatsapp.com/send?phone="+ccode+mobnumber+"&text="+message
+   $('a').get(0).action=href
 }
 
 function resetform(){
