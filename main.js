4555df7c8a4980c38153b4d42cebdb9827eb4110
vinay singh <53850778+bvinays@users.noreply.github.com>
2021-04-22 17:56:26 +0530

    Changes

--- main.js
+++ main.js
@@ -5,8 +5,10 @@ function sendMessage(){
    const ccode=$('#country_list').get(0).value
    const mobnumber=$('#inputMobNum').get(0).value
    const message=$('#inputMessage').get(0).value
+   $('#mobilenumberlength').get(0).innerText="You Entered "+mobnumber.length+" Digits"
    const href="https://web.whatsapp.com/send?phone="+ccode+mobnumber+"&text="+message
    $('a').get(0).href=href
+  
 }
 
 function resetform(){
