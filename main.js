1d7a739b67a8d5a87b3834e4a372f0882594f34a
vinay singh <53850778+bvinays@users.noreply.github.com>
2021-04-22 18:46:39 +0530

    changes

--- main.js
+++ main.js
@@ -6,10 +6,23 @@ function sendMessage(){
    const mobnumber=$('#inputMobNum').get(0).value
    const message=$('#inputMessage').get(0).value
    $('#mobilenumberlength').get(0).innerText="You Entered "+mobnumber.length+" Digits"
-   const href="https://web.whatsapp.com/send?phone="+ccode+mobnumber+"&text="+message
+if(detectMob()){
+   var href="https://wa.me/"+ccode+mobnumber+"?text="+decodeURIComponent(message)
    $('a').get(0).href=href
-  
+        }
+        else{
+            var href="https://api.whatsapp.com/send/?phone="+ccode+mobnumber+"&text="+message+"&app_absent=0"
+            $('a').get(0).href=href
+            
+        }
+
+
 }
+
+function detectMob() {
+    return ( ( window.innerWidth <= 800 ));
+  }
+  
 
 function resetform(){
 
