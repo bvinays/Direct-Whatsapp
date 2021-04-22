283ff7b4f1104ffbc653a7de693f4e44f50a9422
bvinays <53850778+bvinays@users.noreply.github.com>
2021-04-22 15:49:21 +0530

    firstcommit

---
+++ main.js
@@ -0,0 +1,13 @@
+
+
+
+function sendMessage(){
+   const ccode=$('#country_list').get(0).value
+   const mobnumber=$('#inputMobNum').get(0).value
+    window.location.href="https://wa.me/"
+}
+
+function resetform(){
+
+
+}
