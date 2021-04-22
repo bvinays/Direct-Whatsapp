c2a4f73f30ff815c811b871db86b27e2885d4d60
vinay singh <53850778+bvinays@users.noreply.github.com>
2021-04-22 19:08:16 +0530

    final changes

--- main.js
+++ main.js
@@ -1,30 +1,25 @@
 
 
-
 function sendMessage(){
    const ccode=$('#country_list').get(0).value
    const mobnumber=$('#inputMobNum').get(0).value
    const message=$('#inputMessage').get(0).value
-   $('#mobilenumberlength').get(0).innerText="You Entered "+mobnumber.length+" Digits"
-if(detectMob()){
-   var href="https://wa.me/"+ccode+mobnumber+"?text="+decodeURIComponent(message)
-   $('a').get(0).href=href
-        }
-        else{
-            var href="https://api.whatsapp.com/send/?phone="+ccode+mobnumber+"&text="+message+"&app_absent=0"
-            $('a').get(0).href=href
-            
-        }
-
-
+   const href="https://api.whatsapp.com/send/?phone="+ccode+mobnumber+"&text="+message
+   window.location.href=href
 }
 
-function detectMob() {
-    return ( ( window.innerWidth <= 800 ));
-  }
+function getDigits(){
+    const mobnumber=$('#inputMobNum').get(0).value
+    $('#mobilenumberlength').get(0).innerText="You Entered "+mobnumber.length+" Digits"
+}
+
+// function detectMob() {
+//     return ( ( window.innerWidth <= 800 ));
+//   }
   
 
 function resetform(){
-
-
+    $('#inputMessage').get(0).value=''
+    $('#inputMobNum').get(0).value=''
+    getDigits()
 }
